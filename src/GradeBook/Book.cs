2e0cfb1f75fdaadc07c810be140f317ca22f6882
Ben Reynolds-Carr <ben.reynolds-carr@hackney.gov.uk>
2020-01-09 11:38:11 +0000

    created Book class
    * Added constructor method which defines grades
    * created AddGrade method which adds to grades list

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+        public Book()
+        {
+            grades = new List<double>();
+        }
+
+        List<double> grades;
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+
+    }
+}
