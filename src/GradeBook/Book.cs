71eb1c2a1bf2a29af4f8e023aca901860ff2b3ff
Ben Reynolds-Carr <ben.reynolds-carr@hackney.gov.uk>
2020-01-09 13:59:48 +0000

    updated gitignore

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,21 +1,41 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
 {
     class Book
     {
-        public Book()
+        public Book(string name)
         {
             grades = new List<double>();
+            this.name = name;
         }
-
-        List<double> grades;
 
         public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
 
+        public void ShowStats()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            
+            foreach(var number in grades)
+            {
+                highGrade = Math.Max(highGrade, number);
+                lowGrade = Math.Min(lowGrade, number);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The highest grade is {highGrade:N1}");
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
+            Console.WriteLine($"The average grade is {result:N1}");
+        }
+
+        private List<double> grades;
+        private string name;
 
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,20 +7,10 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new Book();
+            var book = new Book("Ben's Grade Book");
             book.AddGrade(89.1);
-
-            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);
-
-            var result = 0.0;
-            foreach(var number in grades)
-            {
-                result += number;
-            }
-
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N2}");
+            book.AddGrade(90.1);
+            book.ShowStats();
         }
     }
 }
